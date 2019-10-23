6f9e0d1c9e91d08cab94a99e8a2f39e3396bca62
Aurora810 <perrucci.aurora97@gmail.com>
2019-10-23 16:41:06 +0200

    ciao

---
+++ src_javaS/basic.js
@@ -0,0 +1,10 @@
+function isEven(a){
+   if typeof((a)!== 'Number'){
+       return undefined;
+   } else if (a % 2 === 0){
+       return true;
+   } else{
+       return undefined;
+   }
+}
+console.log(isEven('ciao'));
